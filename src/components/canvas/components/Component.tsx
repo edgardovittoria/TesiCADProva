e61bda9225523d87a06ec2115f2ad10a1008cbfb
Draco112358 <gianni.frittella@gmail.com>
2021-11-03 10:29:12 +0100

    Component aggiornato con transformControls

--- src/components/canvas/components/Component.tsx
+++ src/components/canvas/components/Component.tsx
@@ -1,24 +1,25 @@
-import React, {MutableRefObject, useEffect, useRef} from 'react';
-import {Object3DNode, ThreeEvent, useThree} from "@react-three/fiber";
-import {TransformControls} from "@react-three/drei";
+import React, { MutableRefObject, useEffect, useRef } from 'react';
+import { TransformControls } from "@react-three/drei";
 import {
     canvasStateSelector,
-    selectComponent, updateBox3,
+    selectComponent,
+    updateBox3,
 } from "../../../store/canvasSlice";
-import {useTransformations} from "../../../hooks/useTransformations";
-import {useDispatch, useSelector} from "react-redux";
-import {ComponentEntity} from "../../../model/ComponentEntity";
-import {FactoryComponent} from '../../factory/FactoryComponent';
-import {useDetectComponentsCollision} from '../../../hooks/useDetectComponentsCollision';
-import {Mesh} from 'three';
-import {FactoryComponent2} from "../../factory/FactoryComponent2";
+import { useTransformations } from "../../../hooks/useTransformations";
+import { useDispatch, useSelector } from "react-redux";
+import { ComponentEntity } from "../../../model/ComponentEntity";
+import { useDetectComponentsCollision } from '../../../hooks/useDetectComponentsCollision';
+import { Mesh } from 'three';
+import { FactoryComponent2 } from "../../factory/FactoryComponent2";
 
 interface ComponentProps {
+    orbit: MutableRefObject<null>
 }
 
 export const Component: React.FC<ComponentProps> = (
     {
         children,
+        orbit
 
     }) => {
 
@@ -26,16 +27,13 @@ export const Component: React.FC<ComponentProps> = (
     const dispatch = useDispatch();
     const canvasState = useSelector(canvasStateSelector);
     const meshRef = useRef(null)
+    const transformation = useRef(null)
     let componentEntity = children as ComponentEntity
-    
-    useDetectComponentsCollision(componentEntity, canvasState)
 
 
-    useEffect(() => {
-        /*if (canvasState.components.length !== 0) {
-            dispatch(selectComponent(componentEntity.keyComponent));
-        }*/
-    }, []);
+    useTransformations(transformation, orbit);
+    //useDetectComponentsCollision(componentEntity, canvasState)
+
 
     useEffect(() => {
         if (meshRef.current) {
@@ -47,7 +45,7 @@ export const Component: React.FC<ComponentProps> = (
             mesh.geometry.computeBoundingBox()
             mesh.geometry.boundingBox?.applyMatrix4(mesh.matrix)
             if (mesh.geometry.boundingBox) {
-                dispatch(updateBox3({key: componentEntity.keyComponent, box3: mesh.geometry.boundingBox}))
+                dispatch(updateBox3({ key: componentEntity.keyComponent, box3: mesh.geometry.boundingBox }))
             }
             //console.log(meshRef.current)
         }
@@ -55,18 +53,60 @@ export const Component: React.FC<ComponentProps> = (
 
 
     return (
-        <>
-            {(componentEntity.isSelected) ?
+
+        (componentEntity.isSelected) ?
+            <TransformControls
+                key={componentEntity.keyComponent}
+                ref={transformation}
+                position={componentEntity.position}
+                rotation={componentEntity.rotation}
+                scale={componentEntity.scale}
+                type={undefined} isGroup={undefined} id={undefined} uuid={undefined}
+                name={undefined}
+                parent={undefined} modelViewMatrix={undefined} normalMatrix={undefined}
+                matrixWorld={undefined} matrixAutoUpdate={undefined}
+                matrixWorldNeedsUpdate={undefined}
+                castShadow={undefined} receiveShadow={undefined} frustumCulled={undefined}
+                renderOrder={undefined} animations={undefined}
+                userData={{ key: componentEntity.keyComponent }}
+                customDepthMaterial={undefined} customDistanceMaterial={undefined}
+                isObject3D={undefined}
+                onBeforeRender={undefined} onAfterRender={undefined}
+                applyMatrix4={undefined}
+                applyQuaternion={undefined} setRotationFromAxisAngle={undefined}
+                setRotationFromEuler={undefined} setRotationFromMatrix={undefined}
+                setRotationFromQuaternion={undefined} rotateOnAxis={undefined}
+                rotateOnWorldAxis={undefined} rotateX={undefined} rotateY={undefined}
+                rotateZ={undefined}
+                translateOnAxis={undefined} translateX={undefined} translateY={undefined}
+                translateZ={undefined} localToWorld={undefined} worldToLocal={undefined}
+                lookAt={undefined} add={undefined} remove={undefined} clear={undefined}
+                getObjectById={undefined} getObjectByName={undefined}
+                getObjectByProperty={undefined}
+                getWorldPosition={undefined} getWorldQuaternion={undefined}
+                getWorldScale={undefined}
+                getWorldDirection={undefined} raycast={undefined} traverse={undefined}
+                traverseVisible={undefined} traverseAncestors={undefined}
+                updateMatrix={undefined}
+                updateWorldMatrix={undefined} toJSON={undefined} clone={undefined}
+                copy={undefined}
+                addEventListener={undefined} hasEventListener={undefined}
+                removeEventListener={undefined}
+                dispatchEvent={undefined} updateMatrixWorld={undefined} visible>
                 <primitive
                     ref={meshRef}
                     object={FactoryComponent2(componentEntity)}
-                /> :
-                <primitive
-                    ref={meshRef}
-                    object={FactoryComponent2(componentEntity)}
-                    position={componentEntity.position}
-                />}
-        </>
+                />
+
+            </TransformControls> :
+
+            <primitive
+                ref={meshRef}
+                object={FactoryComponent2(componentEntity)}
+                position={componentEntity.position}
+                onClick={() => dispatch(selectComponent(componentEntity.keyComponent))}
+            />
+
     )
 
 }
