21d3573d9c5170df06a6b588e99b34809411cfdf
edgardovittoria <edgardovittoria@hotmail.it>
2021-11-22 10:26:55 +0100

    migliorata funzione di export nel formato stl

--- src/auxiliaryFunctionsForImportAndExport/exportFunctions.ts
+++ src/auxiliaryFunctionsForImportAndExport/exportFunctions.ts
@@ -4,6 +4,7 @@ import {STLExporter} from "three/examples/jsm/exporters/STLExporter";
 import * as THREE from "three";
 import {FactoryComponent} from "../components/factory/FactoryComponent";
 import {meshFrom} from "../auxiliaryFunctionsUsingThreeDirectly/meshOpsAndSettings";
+import {ComponentEntity} from "../model/ComponentEntity";
 
 export const exportProjectFrom = (store: Store) => {
     return JSON.stringify(store.getState())
@@ -13,14 +14,21 @@ export const exportToSTLFormatFrom = (canvasState: CanvasState) => {
     let exporter = new STLExporter();
     let scene = new THREE.Scene()
     canvasState.components.map(component => {
-        if (component.box3Min !== undefined && component.box3Max !== undefined) {
-            let mesh = meshFrom(component);
-            mesh.position.set(component.position[0], component.position[1], component.position[2])
-            mesh.scale.set(component.scale[0], component.scale[1], component.scale[2])
-            mesh.rotation.set(component.rotation[0], component.rotation[1], component.rotation[2])
+        let componentToExport: ComponentEntity = component
+        if(canvasState.components.length === 1){
+            componentToExport = {
+                ...component,
+                position: [0,0,0]
+            }
+        }
+        if (componentToExport.box3Min !== undefined && componentToExport.box3Max !== undefined) {
+            let mesh = meshFrom(componentToExport);
+            mesh.position.set(componentToExport.position[0], componentToExport.position[1], componentToExport.position[2])
+            mesh.scale.set(componentToExport.scale[0], componentToExport.scale[1], componentToExport.scale[2])
+            mesh.rotation.set(componentToExport.rotation[0], componentToExport.rotation[1], componentToExport.rotation[2])
             mesh.geometry.computeBoundingBox()
-            mesh.geometry.boundingBox?.min.set(component.box3Min[0], component.box3Min[1], component.box3Min[2])
-            mesh.geometry.boundingBox?.max.set(component.box3Max[0], component.box3Max[1], component.box3Max[2])
+            mesh.geometry.boundingBox?.min.set(componentToExport.box3Min[0], componentToExport.box3Min[1], componentToExport.box3Min[2])
+            mesh.geometry.boundingBox?.max.set(componentToExport.box3Max[0], componentToExport.box3Max[1], componentToExport.box3Max[2])
 
             scene.add(mesh)
             scene.updateWorldMatrix(true, true)
