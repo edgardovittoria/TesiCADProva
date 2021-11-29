7c5748e43a81439b464f19d039c519531a0b6c8c
edgardovittoria <edgardovittoria@hotmail.it>
2021-11-29 19:26:47 +0100

    risolti problemi con le funzioni di export

--- src/auxiliaryFunctionsForImportAndExport/exportFunctions.ts
+++ src/auxiliaryFunctionsForImportAndExport/exportFunctions.ts
@@ -15,24 +15,21 @@ export const exportToSTLFormatFrom = (canvasState: CanvasState) => {
     let scene = new THREE.Scene()
     canvasState.components.map(component => {
         let componentToExport: ComponentEntity = component
-        if(canvasState.components.length === 1){
+        if (canvasState.components.length === 1) {
             componentToExport = {
                 ...component,
-                position: [0,0,0]
+                position: [0, 0, 0]
             }
         }
-        if (componentToExport.box3Min !== undefined && componentToExport.box3Max !== undefined) {
-            let mesh = meshFrom(componentToExport);
-            mesh.position.set(componentToExport.position[0], componentToExport.position[1], componentToExport.position[2])
-            mesh.scale.set(componentToExport.scale[0], componentToExport.scale[1], componentToExport.scale[2])
-            mesh.rotation.set(componentToExport.rotation[0], componentToExport.rotation[1], componentToExport.rotation[2])
-            mesh.geometry.computeBoundingBox()
-            mesh.geometry.boundingBox?.min.set(componentToExport.box3Min[0], componentToExport.box3Min[1], componentToExport.box3Min[2])
-            mesh.geometry.boundingBox?.max.set(componentToExport.box3Max[0], componentToExport.box3Max[1], componentToExport.box3Max[2])
 
-            scene.add(mesh)
-            scene.updateWorldMatrix(true, true)
-        }
+        let mesh = meshFrom(componentToExport);
+        mesh.position.set(componentToExport.position[0], componentToExport.position[1], componentToExport.position[2])
+        mesh.scale.set(componentToExport.scale[0], componentToExport.scale[1], componentToExport.scale[2])
+        mesh.rotation.set(componentToExport.rotation[0], componentToExport.rotation[1], componentToExport.rotation[2])
+
+        scene.add(mesh)
+        scene.updateWorldMatrix(true, true)
+
     })
     return exporter.parse(scene)
 }
