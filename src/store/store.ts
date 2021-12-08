ed9fc2f19a182bc0901a85d8809624d5fe8e4a3f
edgardovittoria <edgardovittoria@hotmail.it>
2021-12-08 12:12:10 +0100

    implementato stato persistente grazie a redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,21 @@
-import { Action,
+import {
+    Action,
     combineReducers,
     configureStore,
-    ThunkAction } from '@reduxjs/toolkit';
+    ThunkAction
+} from '@reduxjs/toolkit';
 import {CanvasSlice} from "./canvasSlice";
 import {ToolbarTransformationSlice} from "./toolbarTransformationSlice";
 import undoable, {excludeAction} from 'redux-undo';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist'
+import persistStore from "redux-persist/es/persistStore";
+
+
+const persistConfig = {
+    key: 'root',
+    storage: storage
+}
 
 const rootReducer = combineReducers({
     canvas: undoable(CanvasSlice.reducer, {
@@ -13,13 +24,17 @@ const rootReducer = combineReducers({
     toolbarTransformation: ToolbarTransformationSlice.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>
